5cada210b037f62b7fd3d69d5672a6ef709c71cb
Askhat Bikmetov <askhat@hey.com>
2025-07-20 04:09:32 +0300

    Refactor `compressCSS` to use `async/await` and improve error handling

--- src/utils/compress.ts
+++ src/utils/compress.ts
@@ -1,34 +1,35 @@
 import { exec } from "node:child_process";
 import fs from "node:fs";
+import { promisify } from "node:util";
 import { resolveTaskRunnerCommand } from "./resolve-task-runner-command";
 
-export function compressCSS(
+const execAsync = promisify(exec);
+
+const taskRunnerCommand = await resolveTaskRunnerCommand();
+
+function isErrorWithMessage(error: unknown): error is Error {
+	return error instanceof Error && "message" in error;
+}
+
+export async function compressCSS(
 	inputFile: string,
 	outputFile: string,
 ): Promise<void> {
-	return new Promise(async (resolve, reject) => {
-		const taskRunner = await resolveTaskRunnerCommand();
-		exec(
-			`${taskRunner} postcss ${inputFile} --use cssnano --no-map -o ${outputFile}`,
-			(error, stdout, stderr) => {
-				if (error) {
-					console.error(`[iconify] Error compressing CSS: ${error.message}`);
-					reject(error);
-					return;
-				}
-				if (stderr) {
-					console.error(`[iconify] stderr: ${stderr}`);
-				}
-				const originalSize = fs.statSync(inputFile).size;
-				const compressedSize = fs.statSync(outputFile).size;
-				const savings = ((1 - compressedSize / originalSize) * 100).toFixed(2);
-				console.log(
-					`[iconify] CSS compressed: ${(originalSize / 1024).toFixed(2)} KB -> ${(
-						compressedSize / 1024
-					).toFixed(2)} KB (saved ${savings}%)`,
-				);
-				resolve();
-			},
+	try {
+		await execAsync(
+			`${taskRunnerCommand} postcss ${inputFile} --use cssnano --no-map -o ${outputFile}`,
 		);
-	});
+		const originalSize = fs.statSync(inputFile).size;
+		const compressedSize = fs.statSync(outputFile).size;
+		const savings = ((1 - compressedSize / originalSize) * 100).toFixed(2);
+		console.log(
+			`[iconify] CSS compressed: ${(originalSize / 1024).toFixed(2)} KB -> ${(
+				compressedSize / 1024
+			).toFixed(2)} KB (saved ${savings}%)`,
+		);
+	} catch (error) {
+		if (isErrorWithMessage(error)) {
+			console.error(`[iconify] Error compressing CSS: ${error.message}`);
+		}
+	}
 }
